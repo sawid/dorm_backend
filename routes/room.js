4be10178d1eaaa3dd5f19d31d8269b8c3a4a6070
wittawas19 <wittawas19@gmail.com>
2022-04-23 18:07:22 +0700

    Update roomdetail

--- routes/room.js
+++ routes/room.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { listRoom, createRoom, readRoom, removeRoom } = require('../controllers/room')
+const { listRoom, createRoom, readRoom, removeRoom ,updateRoom, updatePhoneNumber , updateRoomNameRenter, updateRoomType,updateRentelFee } = require('../controllers/room')
 
 const { auth, adminCheck } = require('../middleware/auth')
 
 router.get('/room', auth, adminCheck, listRoom);
 
 router.get('/room/:id', auth, adminCheck, readRoom);
+
+router.put('/room/:id', auth, adminCheck, updateRoom)
 
 router.post('/room', auth, adminCheck, createRoom);
 
@@ -17,3 +19,14 @@ router.delete('/room/:id', auth, adminCheck, removeRoom);
 
 
 module.exports = router
+
+
+
+
+// Not Use
+/*
+router.put('/room-name-renter/:id', auth, adminCheck, updateRoomNameRenter) 
+router.put('/room-type/:id', auth, adminCheck, updateRoomType)  
+router.put('/room-fee/:id', auth, adminCheck, updateRentelFee) 
+router.put('/room-phone-number/:id', auth, adminCheck, updatePhoneNumber) 
+*/
