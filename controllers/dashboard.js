092e9cd750edc4cc3ed695b359d029669f5b79fb
Sawid Limkiatsataporn <zasawidza@gmail.com>
2022-04-27 23:06:19 +0700

    update dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs')
 const Annoucement = require("../models/Annoucement")
+const Renter = require("../models/Renter")
+const Problem = require("../models/Problem")
+const Bill = require("../models/Bill")
 const jwt = require("jsonwebtoken")
 const { token } = require('morgan')
 
@@ -13,3 +16,35 @@ exports.listAnnoucement = async(req, res) => {
             res.status(500).send("ServerError")
         }
     }
+
+exports.numberTopDashboard = async(req, res) => {
+    try {
+        const numberRenter = await Renter.countDocuments({}).exec();
+        const numberProblem = await Problem.countDocuments({}).exec();
+        const numberNotPay = await Bill.countDocuments({ isPayed: false }).exec();
+        const numberTotal = await Bill.aggregate([
+            {
+              $group: {
+                _id: 1,
+                count: {
+                  $sum: "$rentalNet"
+                }
+              }
+            }
+          ])
+         
+
+        var dataNumber = {
+            numberRenter : numberRenter,
+            numberProblem : numberProblem,
+            numberNotPay : numberNotPay,
+            numberTotal : numberTotal[0].count
+        }
+
+        res.send(dataNumber)
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("ServerError")
+    }
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { listAnnoucement } = require('../controllers/dashboard');
+const { listAnnoucement, numberTopDashboard } = require('../controllers/dashboard');
 const { lineSentBroadcast } = require('../controllers/linebot');
 const router = express.Router()
 
 const { auth, adminCheck } = require('../middleware/auth')
 
 router.get('/annoucement', auth, adminCheck, listAnnoucement);
+
+router.get('/dashboardtopbar', auth, adminCheck, numberTopDashboard);
 
 router.post('/annoucement', auth, adminCheck, lineSentBroadcast);
 
